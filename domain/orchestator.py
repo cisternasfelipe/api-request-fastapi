fa2a67be880142c7db034e57207146d9459577f9
icojjj <felipecisternaspardo@gmail.com>
2026-05-15 23:15:34 -0400

    update Orchestator

--- domain/orchestator.py
+++ domain/orchestator.py
@@ -5,4 +5,5 @@ if TYPE_CHECKING:
     from planner import Planner
 
 class Orchestator(Agent):
-    
+    def __init__(self, name, sys_prompt, tools, knowledge, context_summarize, msg):
+        super().__init__(name, sys_prompt, tools, knowledge, context_summarize, msg)
